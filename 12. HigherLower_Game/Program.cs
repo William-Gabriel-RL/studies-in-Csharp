5cd43bec96d34ec34f4df7e6141ff6eb92c2a11c
William Gabriel <116837882+William-Gabriel-RL@users.noreply.github.com>
2022-11-11 11:53:50 -0300

    Fetch data in exercise 12

--- 12. HigherLower_Game/Program.cs
+++ 12. HigherLower_Game/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿string logo = "\r\n    __  ___       __             \r\n   / / / (_)___ _/ /_  ___  _____\r\n  / /_/ / / __ `/ __ \\/ _ \\/ ___/\r\n / __  / / /_/ / / / /  __/ /    \r\n/_/ ///_/\\__, /_/ /_/\\___/_/     \r\n   / /  /____/_      _____  _____\r\n  / /   / __ \\ | /| / / _ \\/ ___/\r\n / /___/ /_/ / |/ |/ /  __/ /    \r\n/_____/\\____/|__/|__/\\___/_/     ";
+string vs = " _    __    \r\n| |  / /____\r\n| | / / ___/\r\n| |/ (__  ) \r\n|___/____(_)";
+
+Dictionary<string, string> instagram = new Dictionary<string, string>()
+{
+    {"name", "Instagram" },
+    {"follower_count", "346" },
+    {"description", "Social Media Platform" },
+    {"country", "United States" }
+};
+Dictionary<string, string> cristianoRonaldo = new Dictionary<string, string>()
+{
+    {"name", "Cristiano Ronaldo" },
+    {"follower_count", "215" },
+    {"description", "Footballer" },
+    {"country", "Portugal" }
+};
+Dictionary<string, string> arianaGrande = new Dictionary<string, string>()
+{
+    {"name", "Ariana Grande" },
+    {"follower_count", "183" },
+    {"description", "Musician and actress" },
+    {"country", "United States" }
+};
+Dictionary<string, string> dwayneJohnson = new Dictionary<string, string>()
+{
+    {"name", "Dwayne Johnson" },
+    {"follower_count", "181" },
+    {"description", "Actor and professional wrestler" },
+    {"country", "United States" }
+};
+Dictionary<string, string> selenaGomez = new Dictionary<string, string>()
+{
+    {"name", "Selena Gomez" },
+    {"follower_count", "174" },
+    {"description", "Musician and Actress" },
+    {"country", "United States" }
+};
+
+List<Dictionary<string, string>> list = new List<Dictionary<string, string>>() { instagram, cristianoRonaldo, arianaGrande, dwayneJohnson, selenaGomez};
